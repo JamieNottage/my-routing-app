298edae0265599c189304f721128930ce784e44d
Jamie Nottage <jamie.nottage@outlook.com>
2021-11-30 17:25:36 +0000

    Added functionality to pass user id and name from users to user for displaying

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: { id: number; name: string };
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
     console.log('UserComponent Loaded...');
+    this.route.params.subscribe((params) => {
+      this.user = params as { id: number; name: string };
+    });
   }
 
   ngOnInit() {}
